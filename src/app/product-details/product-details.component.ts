46bc041d1ebab42a629423554ee50e7a9d1fc9cf
matekhong <matekhong@hotmail.com>
2019-06-26 15:59:58 +0700

    Add files via upload

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -8,10 +9,22 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
-  constructor() { }
+  constructor(
+    private rout: ActivatedRoute,
+    private carService: CartService,
+  ) { }
 
   ngOnInit() {
+    this.rout.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')]
+    })
+  }
+
+  addToCart(product){
+    window.alert('Your product has been added to the catt!');
+    this.carService.addToCart(product);
   }
 
 }
