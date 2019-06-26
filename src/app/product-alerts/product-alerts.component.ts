27a1e08315841a3214766fe23c276f18efe5f45e
matekhong <matekhong@hotmail.com>
2019-06-26 13:07:57 +0700

    Add files via upload

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,8 +8,8 @@ import { Output, EventEmitter } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product;
-  @Output() notify = new EventEmitter();
+@Input() product;
+@Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
